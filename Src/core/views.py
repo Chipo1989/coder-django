286e16b14ec9635c36066841788da0d00af38690
Giancarlo Cipolatti <gianca_chipo@yahoo.com.ar>
2025-02-25 22:34:04 -0300

    feat: respuesta simple

--- Src/core/views.py
+++ Src/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def saludar(request):
+    return HttpResponse("Hola desde Django!")
+
+def saludar_con_etiqueta(request):
+    return HttpResponse('<h1>Hola desde Django con etiquetas!</h1>')
